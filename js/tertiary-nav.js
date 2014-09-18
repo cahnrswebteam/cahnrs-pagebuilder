027d21f44575050575014d1339a45703eb4a5b7b
Phil Cable <pcable@wsu.edu>
2014-09-18 14:25:46 -0700

    First pass at tab navigation wrap prevention

--- js/tertiary-nav.js
+++ js/tertiary-nav.js
@@ -1,7 +1,9 @@
 jQuery(document).ready(function($) {
-	$( 'body' ).on( 'click', '#pagebuilder-tertiary-nav a', function(event) {
+	$( 'body' ).on( 'click', '#pagebuilder-tertiary-nav a.selected', function(event) {
 		//$( this ).addClass('selected');
 		//$(this).closest('ul').toggleClass('selected');
 		//event.preventDefault();
+		$(this).closest('ul').toggleClass('open');
+		event.preventDefault();
 	});
 });
